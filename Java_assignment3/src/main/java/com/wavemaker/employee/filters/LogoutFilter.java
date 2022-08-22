2ee766db68a6484ff046a25f85467076b6b2e088
kavyayellapragada <110447315+kavyayellapragada@users.noreply.github.com>
2022-08-23 00:07:07 +0530

    Add files via upload
    added filters

---
+++ Java_assignment3/src/main/java/com/wavemaker/employee/filters/LogoutFilter.java
@@ -0,0 +1,19 @@
+package com.wavemaker.employee.filters;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LogoutFilter extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session=request.getSession(false);
+        if(session!=null){
+            session.invalidate();
+        }
+        response.getWriter().write("Logout Successfull");
+    }
+}
